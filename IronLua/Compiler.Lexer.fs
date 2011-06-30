16b1f5aef2bd04b4e26f41d5564fb819e43de78b
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-06-30 15:03:12 +0200

    Moved lexer stuff around. Created lexer function.

--- IronLua/Compiler.Lexer.fs
+++ IronLua/Compiler.Lexer.fs
@@ -4,58 +4,71 @@ module Lexer =
     module Symbol =
         let [<Literal>] EOL = 0
 
-    type State =
-        val mutable Source : string
-        val mutable Index : int
-        val mutable Char : char
-        val mutable Line : int
-        val mutable Column : int
-        val mutable StoredLine : int
-        val mutable StoredColumn : int
-        val mutable Buffer : System.Text.StringBuilder
+    module Input =
+        type State =
+            val mutable Source : string
+            val mutable Index : int
+            val mutable Char : char
+            val mutable Line : int
+            val mutable Column : int
+            val mutable StoredLine : int
+            val mutable StoredColumn : int
+            val mutable Buffer : System.Text.StringBuilder
         
-        new(source) = {
-            Source = source
-            Index = 0
-            Char = '\000'
-            Line = 1
-            Column = 0
-            StoredLine = -1
-            StoredColumn = -1
-            Buffer = System.Text.StringBuilder(1024)
-        }
+            new(source) = {
+                Source = source
+                Index = 0
+                Char = '\000'
+                Line = 1
+                Column = 0
+                StoredLine = -1
+                StoredColumn = -1
+                Buffer = System.Text.StringBuilder(1024)
+            }
 
-    let current (s:State) =
-        s.Source.[s.Index]
+        let create source =
+            State(source)
 
-    let canContinue (s:State) =
-        s.Index < s.Source.Length
+        let current (s:State) =
+            s.Source.[s.Index]
 
-    let advance (s:State) =
-        s.Index <- s.Index + 1
-        s.Column <- s.Column + 1
+        let canContinue (s:State) =
+            s.Index < s.Source.Length
 
-    let newline (s:State) =
-        s.Line <- s.Line + 1
-        s.Column <- 0
+        let advance (s:State) =
+            s.Index <- s.Index + 1
+            s.Column <- s.Column + 1
 
-    let peek (s:State) =
-        s.Source.[s.Index+1]
+        let newline (s:State) =
+            s.Line <- s.Line + 1
+            s.Column <- 0
 
-    let canPeek (s:State) =
-        s.Index+1 < s.Source.Length
+        let peek (s:State) =
+            s.Source.[s.Index+1]
 
-    let bufferAppend (s:State) (c:char) =
-        s.Buffer.Append(c) |> ignore
+        let canPeek (s:State) =
+            s.Index+1 < s.Source.Length
 
-    let bufferClear (s:State) =
-        s.Buffer.Clear() |> ignore
+        let bufferAppend (s:State) (c:char) =
+            s.Buffer.Append(c) |> ignore
 
-    let output (s:State) sym =
-        sym, null, s.StoredLine, s.StoredColumn
+        let bufferClear (s:State) =
+            s.Buffer.Clear() |> ignore
 
-    let outputBuffer (s:State) sym =
-        sym, s.Buffer.ToString(), s.StoredLine, s.StoredColumn
+        let output (s:State) sym =
+            sym, null, s.StoredLine, s.StoredColumn
 
-    let outputEOL (s:State) =
-        Symbol.EOL, null, s.Line, s.Column
+        let outputBuffer (s:State) sym =
+            sym, s.Buffer.ToString(), s.StoredLine, s.StoredColumn
+
+        let outputEOL (s:State) =
+            Symbol.EOL, null, s.Line, s.Column
+
+    open Input
+
+    let create source =
+        let s = Input.create source
+        let rec lexer () =
+            ()
+
+        lexer
