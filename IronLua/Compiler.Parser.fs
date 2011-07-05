c676e7ec0d79df336ba88bad2947f46ff732fd10
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-07-05 22:50:37 +0200

    Added parsing of blocks and begun on statement parsing

--- IronLua/Compiler.Parser.fs
+++ IronLua/Compiler.Parser.fs
@@ -1,6 +1,8 @@
 ﻿namespace IronLua.Compiler
 
 module Parser =
+    type S = Lexer.Symbol
+
     type State =
         val mutable Lexeme : Lexer.Lexeme
         val mutable Lexer : unit -> Lexer.Lexeme
@@ -9,9 +11,50 @@ module Parser =
             Lexeme = Unchecked.defaultof<Lexer.Lexeme>
             Lexer = lexer
         }
+    
+    let inline token (s:State) =
+        let (symbol, _, _, _) = s.Lexeme
+        symbol
 
-    let block s =
+    let consume (s:State) =
+        s.Lexeme <- s.Lexer()
+
+    let inline tryConsume (s:State) symbol =
+        let token = token s
+        if token = symbol then consume s
+
+
+    let explist s =
         failwith ""
+
+    let statement s =
+        failwith ""
+
+    let lastStatement s =
+        match token s with
+        | S.Return -> Some (explist s)
+        | S.Break -> Some Ast.Break
+        | _ -> None
+
+    (* Parses a block
+       {stat [';']} [laststat [';']] *)
+    let block s =
+        let rec block statements =
+            if token s = S.EOF then
+                statements, None
+            else
+                match statement s with
+                | Some stat -> 
+                    tryConsume s S.SemiColon
+                    block (stat :: statements)
+                | None ->
+                    tryConsume s S.SemiColon
+                    let lastStat = lastStatement s
+                    tryConsume s S.SemiColon
+                    statements, lastStat
+
+        block []
+                
 
     let parse source =
         let lexer = Lexer.create source
