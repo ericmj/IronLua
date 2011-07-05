40a8e3b1e5b29378417b3f1a6d0e88e281b00ff8
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-07-05 21:09:47 +0200

    Started work on parser.

--- IronLua/Compiler.Parser.fs
+++ IronLua/Compiler.Parser.fs
@@ -1,3 +1,19 @@
 ﻿namespace IronLua.Compiler
 
-module Parser = ()
+module Parser =
+    type State =
+        val mutable Lexeme : Lexer.Lexeme
+        val mutable Lexer : unit -> Lexer.Lexeme
+
+        new(lexer) = {
+            Lexeme = Unchecked.defaultof<Lexer.Lexeme>
+            Lexer = lexer
+        }
+
+    let block s =
+        failwith ""
+
+    let parse source =
+        let lexer = Lexer.create source
+        let s = State(lexer)
+        block s |> Ast.Block
