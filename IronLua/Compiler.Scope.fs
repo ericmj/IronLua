5fff3fc1885f3d4a348451c0bd2cc435f08db41b
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-08-03 21:47:21 +0200

    Added scope

---
+++ IronLua/Compiler.Scope.fs
@@ -0,0 +1,23 @@
+﻿namespace IronLua.Compiler
+
+open System.Collections.Generic
+
+module Scope =
+    type T =
+        val Parent : T option
+        val Children : T list
+        val Variables : Dictionary<string, ParamExpr>
+
+        new(parent) = {
+            Parent = parent
+            Children = []
+            Variables = new Dictionary<string, ParamExpr>()
+        }
+
+    let createRoot () =
+        T(None)
+
+    let create parent =
+        T(Some parent)
+        
+
