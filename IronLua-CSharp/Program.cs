8e4c273f3bea34f43a485fc393b1d9653dd6cd23
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-08-09 16:05:18 +0200

    Added project and symbol enum

---
+++ IronLua-CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronLua_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
