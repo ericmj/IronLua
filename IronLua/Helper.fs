c4379e888fb1adbb202adb38c18e1ba127b45b45
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-08-09 13:52:41 +0200

    Forgot to add

---
+++ IronLua/Helper.fs
@@ -0,0 +1,13 @@
+﻿namespace IronLua
+
+module Helper =
+    module Array =
+        open Microsoft.FSharp.Collections
+
+        let resize<'a> (array:'a[]) size (value:'a) =
+            if size > Array.length array then
+                Array.append array (Array.create (size - Array.length array) value)
+            elif size < Array.length array then
+                Array.sub array 0 size
+            else
+                array
