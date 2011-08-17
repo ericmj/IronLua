b1bc37cd51a200db84a526934259d4b58e2c1c56
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-08-18 01:01:53 +0200

    Changed IronLua project's output to DLL and created Sample project for easier debugging and testing

--- Sample/Properties/AssemblyInfo.cs
+++ Sample/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IronLua")]
-[assembly: AssemblyDescription("IronLua - A Lua runtime for .NET")]
+[assembly: AssemblyTitle("Sample")]
+[assembly: AssemblyDescription("Sample application for IronLua")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IronLua")]
-[assembly: AssemblyCopyright("Copyright © Eric Meadows-Jönsson 2011")]
+[assembly: AssemblyProduct("Sample")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e3fe6c70-21d3-4ebd-aa13-fbfb0f78b948")]
+[assembly: Guid("b3b9952b-8a38-40c8-a762-5e9524b00a8f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,7 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
-
-[assembly:InternalsVisibleTo("Sample")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
