1c24b5f1d25a7f8cf1265a0414de2802011a3614
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-08-24 00:42:05 +0200

    Removed ListUtil. Added LuaString.

---
+++ IronLua/Library/LuaString.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronLua.Library
+{
+    static class LuaString
+    {
+        public static double[] Byte(string str, int? i = null, int? j = null)
+        {
+            char[] chars;
+            if (!i.HasValue)
+                chars = str.ToCharArray();
+            else if (!j.HasValue)
+                chars = str.Substring(i.Value).ToCharArray();
+            else
+                chars = str.Substring(i.Value, j.Value).ToCharArray();
+
+            return chars.Select(c => (double)c).ToArray();
+        }
+
+        public static string Char(params double[] varargs)
+        {
+            var sb = new StringBuilder(varargs.Length);
+            foreach (var arg in varargs)
+                sb.Append((char) arg);
+            return sb.ToString();
+        }
+
+        public static string Dump(object function)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object[] Find(string str, string pattern, int? init = 1, bool? plain = false)
+        {
+            if (plain.HasValue && plain.Value == true && init.HasValue)
+            {
+                int index = str.Substring(init.Value).IndexOf(pattern);
+                return index != -1 ? new object[] {index, index+pattern.Length} : null;
+            }
+            throw new NotImplementedException();
+        }
+
+        public static string Format(string format, params object[] varargs)
+        {
+            // TODO
+            return String.Format(format, varargs);
+        }
+    }
+}
