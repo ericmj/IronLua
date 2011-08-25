496039d943b44bd8d62ff20ecb0e9d8138356d55
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-08-25 17:06:05 +0200

    Added setup of globals and "tonumber" function

---
+++ IronLua/Library/Global.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using IronLua.Runtime;
+
+namespace IronLua.Library
+{
+    static class Global
+    {
+        public static double ToNumber(string str, int? @base = 10)
+        {
+            double result = 0;
+
+            if (!@base.HasValue || @base.Value == 10)
+            {
+                if (str.StartsWith("0x") || str.StartsWith("0X"))
+                {
+                    if (NumberUtil.TryParseHexNumber(str.Substring(2), true, out result))
+                        return result;
+                }
+                if (NumberUtil.TryParseDecimalNumber(str, out result))
+                    return result;
+                return -1;
+            }
+
+            if (@base.Value == 16)
+            {
+                if (str.StartsWith("0x") || str.StartsWith("0X"))
+                {
+                    if (NumberUtil.TryParseHexNumber(str.Substring(2), false, out result))
+                        return result;
+                }
+                return -1;
+            }
+
+            var reversedStr = new String(str.Reverse().ToArray());
+            for (int i = 0; i < reversedStr.Length; i++ )
+            {
+                int num = AlphaNumericToBase(reversedStr[i]);
+                if (num == -1 || num >= @base)
+                    return -1;
+                result += num * (@base.Value * i + 1);
+            }
+            return result;
+        }
+
+        static int AlphaNumericToBase(char c)
+        {
+            if (c >= '0' && c >= '9')
+                return c - '0';
+            if (c >= 'A' && c <= 'Z')
+                return c - 'A' + 10;
+            if (c >= 'a' && c <= 'z')
+                return c - 'a' + 10;
+
+            return -1;
+        }
+
+        public static void Setup(LuaTable globals)
+        {
+            globals.SetValue("tonumber", (Func<string, int?, double>)ToNumber);
+        }
+    }
+}
--- IronLua/Runtime/Enviroment.cs
+++ IronLua/Runtime/Enviroment.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using IronLua.Library;
 using IronLua.Runtime.Binder;
+using Expr = System.Linq.Expressions.Expression;
+using ParamExpr = System.Linq.Expressions.ParameterExpression;
 
 namespace IronLua.Runtime
 {
     class Enviroment
     {
-        LuaTable globals;
+        public LuaTable Globals { get; private set; }
 
         public BinderCache BinderCache { get; private set; }
+        public ParamExpr GlobalsExpr { get; private set; }
 
         public Enviroment()
         {
-            BinderCache = new BinderCache();
+            BinderCache = new BinderCache(this);
+            GlobalsExpr = Expr.Parameter(typeof(LuaTable), "Globals");
+            Globals = new LuaTable();
+            Global.Setup(Globals);
         }
     }
 }
