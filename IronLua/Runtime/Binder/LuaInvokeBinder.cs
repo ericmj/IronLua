809ed4d4e94c603af92f005312d3849d8d69df2e
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-09-08 14:10:29 +0200

    Don't use LuaFunction, it's unnecessary Remove LuaFunction and use LuaInvokeBinder instead.

--- IronLua/Runtime/Binder/LuaInvokeBinder.cs
+++ IronLua/Runtime/Binder/LuaInvokeBinder.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Dynamic;
 using System.Linq;
+using System.Reflection;
+using IronLua.Util;
 using Expr = System.Linq.Expressions.Expression;
 
 namespace IronLua.Runtime.Binder
@@ -14,7 +16,22 @@ namespace IronLua.Runtime.Binder
 
         public override DynamicMetaObject FallbackInvoke(DynamicMetaObject target, DynamicMetaObject[] args, DynamicMetaObject errorSuggestion)
         {
-            throw new NotImplementedException();
+            // TODO: Optional parameters and passing table for named parameters
+
+            // NOTE: We may only need a type restriction on the first argument since we only need special
+            //       handling for LuaTable when it's the lone argument. Although we may need instance restrict
+            //       on that argument then, or probably instance restrict on the function instead, since we
+            //       only want to restrict parameter names not argument values also
+            var restrictions = target.MergeTypeRestrictions(args);
+
+            var expression =
+                Expr.Convert(
+                    Expr.Invoke(
+                        Expr.Convert(target.Expression, target.LimitType),
+                        args.Select(a => a.Expression)),
+                    typeof(object));
+
+            return new DynamicMetaObject(expression, restrictions);
         }
     }
 }
