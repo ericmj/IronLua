87d39705da0721d756d3270cab4b43087cf280d9
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-09-09 17:35:07 +0200

    Fix spelling error

--- IronLua/ExceptionMessage.cs
+++ IronLua/ExceptionMessage.cs
@@ -10,7 +10,7 @@ namespace IronLua
         public const string UNEXPECTED_EOF = "Unexpected end of file";
         public const string UNEXPECTED_EOS = "Unexpected end of string";
         public const string UNEXPECTED_CHAR = "Unexpected '{0}'";
-        public const string UNKNOWN_PUNCTUATION = "Unknown punctuation '{0}";
+        public const string UNKNOWN_PUNCTUATION = "Unknown punctuation '{0}'";
         public const string INVALID_LONG_STRING_DELIMTER = "Invalid long string delimter '{0}'";
         public const string UNEXPECTED_SYMBOL = "Unexpected symbol '{0}'";
         public const string MALFORMED_NUMBER = "Malformed number '{0}'";
