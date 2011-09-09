98100a18a3560d5e0e3985d949c7658cb5a9d2b4
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-09-09 19:09:14 +0200

    Refactor LuaOps.VarargsAssign Remove some code duplication.

--- IronLua/Runtime/LuaOps.cs
+++ IronLua/Runtime/LuaOps.cs
@@ -32,24 +32,9 @@ namespace IronLua.Runtime
         //       the parameters in generator.
         public static void VarargsAssign(IRuntimeVariables variables, object[] values)
         {
-            int varCount = 0;
-            for (int valueCount = 0; valueCount < values.Length && varCount < variables.Count; valueCount++)
-            {
-                var value = values[valueCount];
-                Varargs varargs;
-
-                if ((varargs = value as Varargs) != null)
-                {
-                    for (int varargsCount = 0;
-                         varargsCount < varargs.Count && varCount < variables.Count;
-                         varargsCount++, varCount++)
-                        variables[varCount] = varargs[varargsCount];
-                }
-                else
-                {
-                    variables[varCount++] = value;
-                }
-            }
+            var variablesArray = VarargsAssign(variables.Count, values);
+            for (int i = 0; i < variables.Count; i++)
+                variables[i] = variablesArray[i];
         }
 
         public static object[] VarargsAssign(int numVariables, object[] values)
