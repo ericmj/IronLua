6f539f2353a21fa1cf9a4156bffe5cf34c90e9a6
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2011-09-13 10:50:20 +0200

    Fix some LuaInvokeBinder bugs This will all be rewritten when I figure out how to do function overloading correctly.

--- IronLua/Runtime/Binder/LuaInvokeBinder.cs
+++ IronLua/Runtime/Binder/LuaInvokeBinder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using IronLua.Util;
 using Expr = System.Linq.Expressions.Expression;
 
@@ -17,8 +18,6 @@ namespace IronLua.Runtime.Binder
 
         public override DynamicMetaObject FallbackInvoke(DynamicMetaObject target, DynamicMetaObject[] args, DynamicMetaObject errorSuggestion)
         {
-            // TODO: Handle passing table as single argument for named parameters
-            
             if (!target.HasValue || args.Any(a => !a.HasValue))
                 return Defer(target, args);
 
@@ -26,13 +25,13 @@ namespace IronLua.Runtime.Binder
                 .MergeTypeRestrictions(target, args)
                 .Merge(BindingRestrictions.GetInstanceRestriction(target.Expression, target.Value));
 
-            var resizeArgs = OverloadArgs(target, args, restrictions);
+            var resizedArgs = OverloadArgs(target, args, ref restrictions);
 
             var expression =
                 Expr.Convert(
                     Expr.Invoke(
                         Expr.Convert(target.Expression, target.LimitType),
-                        resizeArgs),
+                        resizedArgs),
                     typeof(object));
 
             return new DynamicMetaObject(expression, restrictions);
@@ -40,45 +39,52 @@ namespace IronLua.Runtime.Binder
 
         // Pads the argument list with default values if there are less arguments than parameters to the function
         // and removes arguments if there are more arguments than parameters.
-        IEnumerable<Expr> OverloadArgs(DynamicMetaObject target, DynamicMetaObject[] args, BindingRestrictions restrictions)
+        IEnumerable<Expr> OverloadArgs(DynamicMetaObject target, DynamicMetaObject[] args, ref BindingRestrictions restrictions)
         {
             // TODO: Make sure that all args are evaluated, even if args.Length > maxArgs
-            //       check this for LuaFunction also
             // TODO: Smart casting of arguments to match parameter type, do value coercion, cast to object if needed etc.
             var function = (Delegate)target.Value;
             var parameters = function.Method.GetParameters();
             var minArgs = parameters.Count(arg => !arg.IsOptional);
             var maxArgs = parameters.Length;
 
+            bool isDynamicLambda = parameters[0].ParameterType == typeof(Closure);
 
-            if (args.Length == 0)
-                return new Expr[] { };
+            if (isDynamicLambda)
+            {
+                minArgs--;
+                maxArgs--;
+            }
 
-            var argExprs = LuaInvokeBinder.ExpandVarargs(args, restrictions).ToList();
+            var argExprs = ExpandVarargs(args, ref restrictions).ToList();
 
             if (argExprs.Count < minArgs)
                 return null; // TODO
 
             var defaultValues = parameters
-                .Skip(argExprs.Count)
+                .Skip(argExprs.Count + (isDynamicLambda ? 1 : 0))
                 .Select(param => Expr.Constant(param.DefaultValue));
 
             // TODO: Check that the types of final args matches the target argument types
             return argExprs.Resize(maxArgs, defaultValues);
         }
 
-        public static IEnumerable<Expr> ExpandVarargs(DynamicMetaObject[] args, BindingRestrictions restrictions)
+        public static IEnumerable<Expr> ExpandVarargs(DynamicMetaObject[] args, ref BindingRestrictions restrictions)
         {
             var argExprs = args
                 .Take(args.Length - 1)
                 .Select(TryWrapWithVarargsSelect);
+
+            if (args.Length == 0)
+                return new Expr[0];
 
             var lastArg = args.Last();
             if (lastArg.LimitType == typeof(Varargs))
             {
                 var varargs = (Varargs)lastArg.Value;
                 argExprs = argExprs.Concat(ExpandVarargsIntoExprs(varargs));
-                BindingRestrictions.GetInstanceRestriction(lastArg.Expression, lastArg.Value).Merge(restrictions);
+                restrictions = restrictions.Merge(
+                    BindingRestrictions.GetInstanceRestriction(lastArg.Expression, lastArg.Value).Merge(restrictions));
             }
             else
             {
