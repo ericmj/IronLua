6b281d8860bb18b6d2790a81c9f0aed551d0df95
Francois Retief <fgretief@gmail.com>
2012-04-22 00:59:07 +0200

    Removed the dependency on Input class.

--- IronLua/LuaSyntaxException.cs
+++ IronLua/LuaSyntaxException.cs
@@ -24,21 +24,6 @@ namespace IronLua
             Column = column;
         }
 
-        internal LuaSyntaxException(Input input, string message, Exception inner = null)
-            : this(input.File, input.Line, input.Column, message, inner)
-        {
-        }
-
-        internal LuaSyntaxException(Input input, string format, params object[] args)
-            : this(input, String.Format(format, args))
-        {
-        }
-
-        internal LuaSyntaxException(Input input, Exception inner, string format, params object[] args)
-            : this(input, String.Format(format, args), inner)
-        {
-        }
-
         protected LuaSyntaxException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
