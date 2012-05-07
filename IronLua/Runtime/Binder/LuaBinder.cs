5e2d0ff3e0f3b7d65f0b84f00f45f049cbacb68e
Francois Retief <fgretief@gmail.com>
2012-05-07 19:49:23 +0200

    Added LuaOverloadResolverFactory and LuaOverloadResolver classes.

--- IronLua/Runtime/Binder/LuaBinder.cs
+++ IronLua/Runtime/Binder/LuaBinder.cs
@@ -1,22 +1,64 @@
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Dynamic;
 using Microsoft.Scripting.Actions;
-using Microsoft.Scripting.Utils;
+using Microsoft.Scripting.Actions.Calls;
+using Microsoft.Scripting.Runtime;
 
 namespace IronLua.Runtime.Binder
 {
     class LuaBinder : DefaultBinder
     {
-        readonly LuaContext _context;
+        private readonly LuaContext _context;
 
         public LuaBinder(LuaContext context)
         {
-            ContractUtils.RequiresNotNull(context, "context");
-
+            Contract.Requires(context != null);
             _context = context;
         }
 
         public LuaBinder(LuaBinder binder)
         {
             _context = binder._context;
+        }
+    }
+
+    internal sealed class LuaOverloadResolverFactory : OverloadResolverFactory
+    {
+        private readonly LuaBinder _binder;
+
+        public LuaOverloadResolverFactory(LuaBinder binder)
+        {
+            Contract.Requires(binder != null);
+            _binder = binder;
+        }
+
+        public override DefaultOverloadResolver CreateOverloadResolver(IList<DynamicMetaObject> args, CallSignature signature, CallTypes callType)
+        {
+            return new LuaOverloadResolver(_binder, args, signature, callType);
+        }
+    }
+
+    internal sealed class LuaOverloadResolver : DefaultOverloadResolver
+    {
+        public LuaOverloadResolver(ActionBinder binder, DynamicMetaObject instance, IList<DynamicMetaObject> args, CallSignature signature) 
+            : base(binder, instance, args, signature)
+        {
+        }
+
+        public LuaOverloadResolver(ActionBinder binder, IList<DynamicMetaObject> args, CallSignature signature) 
+            : base(binder, args, signature)
+        {
+        }
+
+        public LuaOverloadResolver(ActionBinder binder, IList<DynamicMetaObject> args, CallSignature signature, CallTypes callType) 
+            : base(binder, args, signature, callType)
+        {
+        }
+
+        private new LuaBinder Binder
+        {
+            get { return (LuaBinder)base.Binder; }
         }
     }
 }
