16717e753505aecc378a9a09ef1658ad903b7a16
Benjamin Pannell <benjamin.pannell@gmail.com>
2012-09-15 14:52:14 +0200

    Added InteropLibTests script
    Short test script which demonstrates ways in which IronLua's CLR
    interface can be used. Also tests functionality, such as Static method
    calls, Instance method calls, Array initialization and access, Imports
    and more.

---
+++ IronLua.Tests/Scripts/InteropLibTests.lua
@@ -0,0 +1,23 @@
+﻿print("Testing IronLua's CLR implementation")
+
+system = {}
+
+system.convert = clr.import('System.Convert')
+assert(system.convert,'Failed to import System.Convert')
+
+system.int = clr.import('System.Int32')
+assert(system.int,'Failed to import System.Int32')
+
+system.short = clr.import('System.Int16')
+assert(system.short,'Failed to import System.Int16')
+
+print("Attempting: Convert.ToInt32(12)")
+num1 = system.convert.ToInt32(12)
+assert(tostring(num1) == '12',"Failed to convert double to Int32")
+print "    Success"
+
+print("Attempting to create an array: Int32[10]")
+array = clr.makearray(system.int, 10)
+assert(array,'Failed to create array')
+assert(#array == 10,'Array length was not correct')
+array[0]=10
