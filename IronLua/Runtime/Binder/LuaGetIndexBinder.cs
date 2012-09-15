3f42a3ab409c71c650f4ffac80743975882751bf
Benjamin Pannell <benjamin.pannell@gmail.com>
2012-09-15 10:38:58 +0200

    Updated LuaGetIndexBinder
    Added support for getting indexed properties if they are available on an
    object, otherwise fall back on using IronLua's default fallback methods.

--- IronLua/Runtime/Binder/LuaGetIndexBinder.cs
+++ IronLua/Runtime/Binder/LuaGetIndexBinder.cs
@@ -1,5 +1,10 @@
 using System.Diagnostics.Contracts;
 using System.Dynamic;
+using System.Reflection;
+using System.Linq;
+using System;
+using Microsoft.Scripting.Actions;
+using System.Linq.Expressions;
 
 namespace IronLua.Runtime.Binder
 {
@@ -24,11 +29,27 @@ namespace IronLua.Runtime.Binder
             get { return _context; }
         }
         
+        private DynamicMetaObject WrapToObject(DynamicMetaObject obj)
+        {
+            if (obj.LimitType != typeof(object))
+                return new DynamicMetaObject(Expression.Convert(obj.Expression, typeof(object)), obj.Restrictions, obj.Value as object);
+            return obj;
+        }
+
         public override DynamicMetaObject FallbackGetIndex(DynamicMetaObject target, DynamicMetaObject[] indexes, DynamicMetaObject errorSuggestion)
         {
+            if (target.LimitType.GetProperties().Any(x => x.GetIndexParameters().Length == CallInfo.ArgumentCount))
+            {
+                DynamicMetaObject[] args = new DynamicMetaObject[indexes.Length + 1];
+                args[0] = target;
+                Array.Copy(indexes, 0, args, 1, indexes.Length);
+                return WrapToObject(Context.Binder.MakeCallExpression(DefaultOverloadResolver.Factory, target.LimitType.GetMethod("get_Item"), args));
+            }
+
             var expression = MetamethodFallbacks.Index(_context, target, indexes);
 
-            return new DynamicMetaObject(expression, BindingRestrictions.Empty);
+            return WrapToObject(new DynamicMetaObject(expression, BindingRestrictions.Empty));
+            
         }
     }
 }
