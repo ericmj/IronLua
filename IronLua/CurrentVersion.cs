814036645d711b3286bae806c026ab922e358293
Francois Retief <fgretief@gmail.com>
2012-09-20 01:04:37 +0200

    Added generation of version numbers
    Added the ability to generate version numbers for assemblies
    and packages based on the CurrentVersion.props when building
    with the Build.IronLua.proj project file.

---
+++ IronLua/CurrentVersion.cs
@@ -0,0 +1,9 @@
+namespace IronLua
+{
+    /// <summary>
+    /// Helper class containing generated version numbers.
+    /// </summary>
+    public static partial class CurrentVersion 
+    {
+    }
+}
--- IronLua/Properties/AssemblyInfo.cs
+++ IronLua/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("IronLua")]
 [assembly: AssemblyDescription("IronLua - A Lua runtime for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("IronLua Team")]
 [assembly: AssemblyProduct("IronLua")]
-[assembly: AssemblyCopyright("Copyright © Eric Meadows-Jönsson 2011")]
+[assembly: AssemblyCopyright("IronLua Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+
+[assembly: AssemblyVersion(IronLua.CurrentVersion.AssemblyVersion)]
+[assembly: AssemblyFileVersion(IronLua.CurrentVersion.AssemblyFileVersion)]
+[assembly: AssemblyInformationalVersion(IronLua.CurrentVersion.AssemblyInformationalVersion)]
 
 [assembly:InternalsVisibleTo("IronLua.Tests")]
